7422c3e335432a9e7169c02ba431412a65b7c4c2
Kishan kumar Prajapati <kishan.kumar@neosoftmail.com>
2023-05-01 19:15:32 +0530

    Home Screen and Product Screen

---
+++ MERN-AMAZONA-KISHAN-master/src/App.js
@@ -0,0 +1,23 @@
+import data from './data';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link href='#'>Amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path='/product/:slug' element={<ProductScreen />} />
+            <Route path='/' element={<HomeScreen />} />
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
